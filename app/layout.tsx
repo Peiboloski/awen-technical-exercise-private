4fd26bca911b700fe923959792c433cc16e9692c
Pablo D <padelgar@gmail.com>
2024-04-20 18:32:35 +0200

    chore: startup project, install nextUI and add font Lato

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const latoFont = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+  fallback: ["Roboto", "sans-serif"],
+  variable: '--font-Lato',
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={latoFont.className}>{children}
+
+      </body>
     </html>
   );
 }
