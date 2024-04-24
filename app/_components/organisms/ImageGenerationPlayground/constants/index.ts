4384bfc3b86592ffa38ce33f1ea72eaa85adc75b
Pablo D <padelgar@gmail.com>
2024-04-24 14:56:45 +0200

    feat: add full resolution to images

--- app/_components/organisms/ImageGenerationPlayground/constants/index.ts
+++ app/_components/organisms/ImageGenerationPlayground/constants/index.ts
@@ -12,47 +12,47 @@ const INPUT_NAMES = {
 const RESOLUTIONS = {
     "1:1": {
         value: "1:1",
-        width: 1024 / 2,
-        height: 1024 / 2,
+        width: 1024,
+        height: 1024,
         drawingStyles: "w-[20px] h-[20px]",
     },
     "4:5": {
         value: "4:5",
-        width: 1024 / 2,
-        height: 1280 / 2,
+        width: 1024,
+        height: 1280,
         drawingStyles: "w-[16px] h-[20px]"
 
     },
     "2:3": {
         value: "2:3",
-        width: 1024 / 2,
-        height: 1536 / 2,
+        width: 1024,
+        height: 1536,
         drawingStyles: "w-[14px] h-[21px]"
     },
     "4:7": {
         value: "4:7",
-        width: 1024 / 2,
-        height: 1792 / 2,
+        width: 1024,
+        height: 1792,
         drawingStyles: "w-[12px] h-[21px]"
     },
     "5:4": {
         value: "5:4",
-        width: 1280 / 2,
-        height: 1024 / 2,
+        width: 1280,
+        height: 1024,
         drawingStyles: "w-[25px] h-[20px]"
     }
     ,
     "3:2": {
         value: "3:2",
-        width: 1536 / 2,
-        height: 1024 / 2,
+        width: 1536,
+        height: 1024,
         drawingStyles: "w-[30px] h-[20px]"
     }
     ,
     "7:4": {
         value: "7:4",
-        width: 1792 / 2,
-        height: 1024 / 2,
+        width: 1792,
+        height: 1024,
         drawingStyles: "w-[35px] h-[20px]"
     },
 } satisfies Record<string, { value: string, width: number, height: number, drawingStyles?: string }>
